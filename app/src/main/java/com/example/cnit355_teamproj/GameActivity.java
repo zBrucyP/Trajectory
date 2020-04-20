fa285e8e46d6637bbc73f8d098cf8742f832f253
myxxmikeyxx <jimmy98719871@gmail.com>
2020-04-20 11:54:11 -0400

    Landscape Only

--- app/src/main/java/com/example/cnit355_teamproj/GameActivity.java
+++ app/src/main/java/com/example/cnit355_teamproj/GameActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
